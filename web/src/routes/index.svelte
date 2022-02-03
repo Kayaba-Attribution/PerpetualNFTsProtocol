147e42ebf8617a1acf5cc11896b9e7aad410ae51
Kayaba Attribution <jdavidgomezca@gmail.com>
2022-02-03 17:30:45 -0500

    Add gif and ui index

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -60,7 +60,7 @@
 			<h1 class="text-4xl font-bold text-gray-800 md:text-4xl">Perpetual NFTs</h1>
 			<h1 class="text-2xl font-bold text-gray-800 md:text-4xl">Buy, Sell, Earn, Loan</h1>
 			
-			<p class="mt-6 text-gray-500 ">
+			<p class="mt-6 text-gray-800 ">
 				Welcome to the DeFi Matic Gallery. Make your Unique Art NFTs work for you, all while having a value warranty.
 			</p>
 			<div class="py-3 flex justify-center ..." >
@@ -92,7 +92,7 @@
 						</svg>
 					</div>
 				</div>
-				<h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+				<h3 class="text-2xl sm:text-xl text-gray-300 font-semibold  py-4">
 					Guaranteed Value
 				</h3>
 				<p class="text-md  text-gray-500 dark:text-gray-300 py-4">
@@ -108,7 +108,7 @@
 						</svg>
 					</div>
 				</div>
-				<h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+				<h3 class="text-2xl sm:text-xl text-gray-300 font-semibold  py-4">
 					Earn
 				</h3>
 				<p class="text-md text-gray-500 dark:text-gray-300 py-4">
@@ -125,10 +125,10 @@
 						</svg>
 					</div>
 				</div>
-				<h3 class="text-2xl sm:text-xl text-gray-700 font-semibold dark:text-white py-4">
+				<h3 class="text-2xl sm:text-xl text-gray-300 font-semibold  py-4">
 					Loan
 				</h3>
-				<p class="text-md  text-gray-500 dark:text-gray-300 py-4">
+				<p class="text-md  text-gray-800 dark:text-gray-300 py-4">
 					Want to earn more? You can take a loan on top of your deposited NFTs! Up the 50% of the value. Deposit, Loan, Earn, Repeat!
 				</p>
 			</div>
@@ -174,8 +174,126 @@
 		</section>
 	</div>
 
-	
 
+	<section class="bg-transparent">
+		<div class="container px-6 py-10 mx-auto">
+			<h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl ">How does it<br>Work?</h1>
+			
+			<div class="mt-2">
+				<span class="inline-block w-40 h-1 rounded-full bg-blue-500"></span>
+				<span class="inline-block w-3 h-1 ml-1 rounded-full bg-blue-500"></span>
+				<span class="inline-block w-1 h-1 ml-1 rounded-full bg-blue-500"></span>
+			</div>
+			
+			<div class="mt-8 xl:mt-12 lg:flex lg:items-center">
+				<div class="w-full lg:w-1/2 grid grid-cols-1 gap-8 xl:gap-16 md:grid-cols-2">
+					<div class="space-y-3">
+						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+							</svg>
+						</span>
+
+						<h1 class="text-2xl font-semibold text-gray-700 capitalize ">Treasury</h1>
+
+						<p class="text-gray-800">
+							All the mint funds are kept safe in the gallery trasury and streamed to AAVE pools to generate value.
+						</p>
+					</div>
+
+					<div class="space-y-3">
+						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+							</svg>
+						</span>
+
+						<h1 class="text-2xl font-semibold text-gray-700 capitalize ">AAVE</h1>
+
+						<p class="text-gray-800 ">
+							We choose AAVE pools due to their industry standard security and reliablity, we deposit MATIC and get aMATIC in return.
+						</p>
+					</div>
+
+					<div class="space-y-3">
+						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
+							</svg>
+						</span>
+
+						<h1 class="text-2xl font-semibold text-gray-700 capitalize ">Loans</h1>
+
+						<p class="text-gray-800 ">
+							Created a loan system from scratch to use the NFTs value as collateral, in which debt calculation, borrows, repayments and liquidations occur.
+						</p>
+					</div>
+
+					<div class="space-y-3">
+						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
+							</svg>
+						</span>
+
+						<h1 class="text-2xl font-semibold text-gray-700 capitalize ">Staking</h1>
+
+						<p class="text-gray-500 ">
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident ab nulla quod dignissimos vel non corrupti doloribus voluptatum eveniet
+						</p>
+					</div>
+				</div>
+
+				<div class="hidden lg:flex lg:w-1/2 lg:justify-center">
+					<img class="w-[28rem] h-[28rem] flex-shrink-0 object-cover xl:w-[34rem] xl:h-[34rem] rounded-full" src="src/lib/images/maticgif.gif" alt="">
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<p class="pt-5 pb-10 text-center text-3xl font-bold text-gray-800 ">
+		Our Team
+	</p>
+	<div class="md:flex gap-8">
+
+		<div class="md:w-1/2 text-center mb-8 md:mb-0">
+			<img class="w-48 h-48 rounded-full mx-auto -mb-24" src="/images/person/6.jpg" alt="Avatar Jacky"/>
+			<div class="bg-white shadow-lg rounded-lg px-8 pt-32 pb-10 text-gray-400">
+				<h3 class="font-title text-gray-800 text-xl mb-3">
+					0x4non
+				</h3>
+				<p class="font-body">
+					--
+				</p>
+				<p class="font-body text-sm mb-4">
+					He love caramel and he hate PHP
+				</p>
+				<a class="font-body text-blue-500 hover:text-gray-800" href="#">
+					0x4non
+				</a>
+			</div>
+		</div>
+		<div class="md:w-1/2 text-center">
+
+			<img class="w-48 h-48 rounded-full mx-auto -mb-24" src="src/lib/images/kayabaProfile.jpg" alt="Avatar Damien Marley"/>
+			<div class="bg-white shadow-lg rounded-lg px-8 pt-32 pb-10 text-gray-400">
+				<h3 class="font-title text-gray-800 text-xl mb-3">
+					Kayaba_Attribution
+				</h3>
+				<p class="font-body">
+					--
+				</p>
+				<p class="font-body text-sm mb-4">
+					He&#x27;s fun and listen everyday Bob Marley
+				</p>
+				<a class="font-body text-blue-500 hover:text-gray-800" href="mailto:dino@siete.pm">
+					Kayaba_Attribution
+				</a>
+			</div>
+		</div>
+	</div>
+	
 	<h1>HOLA {$wallet}	</h1>
 
 	{#if $wrongNetwork}
