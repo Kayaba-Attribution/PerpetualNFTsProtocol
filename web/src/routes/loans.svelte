14654685192d9cf061b5c73afbcbcccdec498790
eugenioclrc <eugenioclrc@gmail.com>
2022-02-05 14:45:08 -0300

    embellezco botones

--- web/src/routes/loans.svelte
+++ web/src/routes/loans.svelte
@@ -134,9 +134,15 @@ function  BigNumFixed(value, n) {
 	</div>
 
 	<div class="flex flex-grow justify-center my-4">
-		<button class="rounded border bg-white mx-2 px-4 py-2">Take a loan</button>
-		<button class="rounded border bg-white mx-2 px-4 py-2">Repay loan</button>
-		<button class="rounded border bg-white mx-2 px-4 py-2">Widthdraw collateral</button>
+		<button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+			Take a loan
+		</button>
+		<button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+			Repay loan
+		</button>
+		<button type="button" class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:shadow-lg font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+			Widthdraw collateral
+		</button>
 	</div>
 
 	<!-- 
