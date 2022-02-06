8cf82c591881d77256a11933ea8c86103f879302
eugenioclrc <eugenioclrc@gmail.com>
2022-02-06 12:26:37 -0300

    refactor nftValue

--- hardhat/contracts/Perpetual.sol
+++ hardhat/contracts/Perpetual.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-
+import "@openzeppelin/contracts/utils/Address.sol";
 import "./Treasury.sol";
 
 contract Perpetual is ERC721, ERC721Enumerable, Ownable {
@@ -16,6 +16,8 @@ contract Perpetual is ERC721, ERC721Enumerable, Ownable {
 
     Counters.Counter private _tokenIdCounter;
 
+     mapping (uint256 => uint256) private _nftValue;
+
     // address public treasury;
 
     constructor(address payable _treasury) ERC721("Perpetual", "PERP") {
@@ -23,18 +25,24 @@ contract Perpetual is ERC721, ERC721Enumerable, Ownable {
         treasury = Treasury(_treasury);
     }
 
-    function nftValue(uint256 _id) public pure returns(uint256) {
+    function nftValue(uint256 tokenId) public view returns(uint256) {
         // default value
-        return 1 ether;
+        // return 1 ether;
+        return _nftValue[tokenId];
     }
 
     function mint() public payable {
         uint256 tokenId = _tokenIdCounter.current();
         require(tokenId < 99, "No more than 100 Perpetuals can be minted");
-        require(msg.value == nftValue(tokenId), "Must send at least 1 ether");
+        require(msg.value >= 1 ether, "Must send at least 1 ether");
+        treasury.depositAAVE{value: 1 ether}();
         _tokenIdCounter.increment();
+        _nftValue[tokenId] = 1 ether;
         _safeMint(msg.sender, tokenId);
-        treasury.depositAAVE{value: msg.value}();
+        if (msg.value < 1 ether) {
+            uint256 _changeBack = msg.value - 1 ether;
+            Address.sendValue(payable(msg.sender), _changeBack);
+        }
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
