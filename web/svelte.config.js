d00c7a0573596f2e4afee951cddbb6f0c52b911a
Kayaba Attribution <jdavidgomezca@gmail.com>
2022-02-06 18:51:48 -0500

    Vercel Fix

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -44,7 +44,7 @@ const ghpagesConfig = {
     // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
     // For example, instead of '_app', use 'app_', 'internal', etc.
     // appDir: 'internal',
-    target: "#svelte",
+    //target: "#svelte",
 	},
 }
 console.log({GHPAGES})
