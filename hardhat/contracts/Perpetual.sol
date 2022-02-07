ade93482f6bc430af568539ea8d043e11d1d634f
eugenioclrc <eugenioclrc@gmail.com>
2022-02-07 13:41:28 -0300

    comentos console.sol (no se usa mas)

--- hardhat/contracts/Perpetual.sol
+++ hardhat/contracts/Perpetual.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.2;
 
-import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
