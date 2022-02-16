3ad89c48915ebb4933d8177975a8cee1df3f9485
Kayaba Attribution <jdavidgomezca@gmail.com>
2022-02-16 15:44:05 -0500

    feat: see add repo and showcase links

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -168,19 +168,24 @@
 
 	<section class="bg-transparent">
 		<div class="container px-6 py-10 mx-auto">
-			<h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl ">How does it<br>Work?</h1>
+			<h3 class="text-4xl font-semibold text-gray-800 capitalize lg:text-4xl ">
+				How does it Work?
+			</h3>
 			
 			<div class="mt-2">
 				<span class="inline-block w-40 h-1 rounded-full bg-blue-500"></span>
 				<span class="inline-block w-3 h-1 ml-1 rounded-full bg-blue-500"></span>
 				<span class="inline-block w-1 h-1 ml-1 rounded-full bg-blue-500"></span>
 			</div>
+
+			<p>Check Out Github Repo <a href="">Here</a></p>
+			<p>ETH Global Hackaton <a href="https://showcase.ethglobal.com/roadtoweb3/perpetual-nfts-protocol">Showcase</a></p>
 			
-			<div class="mt-8 xl:mt-12 lg:flex lg:items-center">
+			<div class="mt-2 xl:mt-2 lg:flex lg:items-center">
 				<div class="w-full lg:w-1/2 grid grid-cols-1 gap-8 xl:gap-16 md:grid-cols-2">
 					<div class="space-y-3 shadow-lg rounded-lg px-3 py-2 bg-zinc-50">
 						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
 							</svg>
 						</span>
@@ -194,7 +199,7 @@
 
 					<div class="space-y-3 shadow-lg rounded-lg px-3 py-2 bg-zinc-50">
 						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
 							</svg>
@@ -209,7 +214,7 @@
 
 					<div class="space-y-3 shadow-lg rounded-lg px-3 py-2 bg-zinc-50">
 						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z" />
 							</svg>
 						</span>
@@ -223,7 +228,7 @@
 
 					<div class="space-y-3 shadow-lg rounded-lg px-3 py-2 bg-zinc-50">
 						<span class="inline-block p-3 text-blue-500 bg-blue-100 rounded-xl  dark:bg-blue-500">
-							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+							<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="white" viewBox="0 0 24 24" stroke="currentColor">
 								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z" />
 							</svg>
 						</span>
@@ -258,7 +263,7 @@
 					Founder
 				</p>
 				<p class="font-body text-sm mb-4">
-					OG, he love caramel and he hate PHP
+					20 years of web2, founder and CTO of a top meta travel search engine
 				</p>
 				<a class="font-body text-blue-500 hover:text-gray-800" target="_blank" href="https://github.com/eugenioclrc/">
 					0x4non
@@ -276,7 +281,7 @@
 					Co-Founder
 				</p>
 				<p class="font-body text-sm mb-4">
-					He&#x27;s fun and listen everyday Bob Marley
+					CS Student, Full-Stack Blockchain Development
 				</p>
 				<a class="font-body text-blue-500 hover:text-gray-800" target="_blank" href="https://github.com/Kayaba-Attribution">
 					Kayaba_Attribution
